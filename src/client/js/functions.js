46f2af2a74d3f84b707e32476053662e05b1983f
Mikael Wallin <wallindev@gmail.com>
2019-07-01 16:44:41 +0200

    Removed unused code

--- src/client/js/functions.js
+++ src/client/js/functions.js
@@ -1,12 +1,7 @@
-// App initialization
-const init = () => {
-  // Restore all elements
-  global.$chatStatusText.removeClass().addClass('text-warning');
-  global.$textarea.val('');
-  global.$textarea.attr('disabled', true);
-  global.$chatName.val('');
-  global.$chatName.focus();
-};
+import conf from './config';
+
+// TODO:
+const l = console.log;
 
 /* To prevent users from emitting "dangerous" code to the server
   *
@@ -16,41 +11,64 @@ const init = () => {
   * - Replace '<' with '&lt;'
   * - Replace '>' with '&gt;'
   */
-const htmlspecialchars = str => {
+export const htmlspecialchars = str => {
   str.replace(/&/gim, '&amp;').replace(/"/gim, '&quot;').replace(/'/gim, '&#039;').replace(/</gim, '&lt;').replace(/>/gim, '&gt;')
 };
 
-// Sets status text at bottom or beside nick
-const setStatus = (msg, type, which, clear, restore) => {
+// Sets status text at bottom or beside name
+// CSS class 'text-warning' (from Bootstrap) is in spite of its name used for normal/idle mode,
+// because the color '#8a6d3b' is suitable in this context for neutral notifications.
+export const setStatus = (msg, type = 'info', which = 'status', clear = false, restore = true) => {
   if (msg === undefined) {
     console.error("msg can't be empty");
     return 1;
   }
-  if (type === undefined) type = 'info';
-  if (which === undefined) which = 'status';
-  if (clear === undefined) clear = false;
-  if (restore === undefined) restore = true;
 
-  if (which === 'nick') {
-    global.$chatNameMsg.removeClass().addClass('text-' + type);
-    global.$chatNameMsg.html(msg);
+  // TODO: Solve this with refs somehow?
+  const lblName = document.getElementById('chat-name-msg');
+  const lblStatus = document.getElementById('chat-status');
+
+  if (which === 'name') {
+    lblName.classList = `text-${type}`;
+    lblName.innerHTML = msg;
   } else {
-    global.$chatStatusText.removeClass().addClass('text-' + type);
-    global.$chatStatusText.html(msg);
+    lblStatus.classList = `text-${type}`;
+    lblStatus.innerHTML = msg;
 
     // Reset status message after 5 seconds
     if (restore) {
-      if (msg !== global.STATUS) {
+      if (msg !== conf.STATUS) {
         setTimeout(() => {
           // TODO: Fix ES6 Version (Recursive function call)
           // var self = this;
-          // self.setStatus(global.STATUS, 'warning');
-          global.$chatStatusText.removeClass().addClass('text-warning');
-          global.$chatStatusText.html(global.STATUS);
+          this.setStatus(conf.STATUS, 'warning');
+          // lblStatus.classList = 'text-warning';
+          // lblStatus.innerHTML = conf.STATUS;
         }, 5000);
       }
     }
   }
   if (clear)
-    global.$textarea.val('');
+    txtMessage.value = '';
 };
+
+export const clearStatus = () => {
+  setStatus('', 'warning', 'name');
+}
+
+export const toggleTxtMessage = (disable = null) => {
+  // TODO: Solve this with refs somehow?
+  const txtMessage = document.getElementById('chat-textarea');
+
+  // If forced with 'disable' argument
+  l('disable:', disable);
+  if (disable !== null) {
+    disable ? txtMessage.setAttribute('disabled', 'disabled') : txtMessage.removeAttribute('disabled');
+    return;
+  }
+
+  // If not forced, toggle disabled state
+  const isDisabled = txtMessage.getAttribute('disabled');
+  l('isDisabled:', isDisabled);
+  isDisabled ? txtMessage.removeAttribute('disabled') : txtMessage.setAttribute('disabled', 'disabled');
+}
