f1ed426f49c27c7ff7a95281afab09f326980b98
Mikael Wallin <wallindev@gmail.com>
2019-07-01 18:33:20 +0200

    Changed Users component to stateless function

--- src/client/components/Users.jsx
+++ src/client/components/Users.jsx
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-const noUsers = <div ng-show="!users.length">
-  No users
-</div>;
+const Users = ({ heading, users }) => {
+  const divUsers = users.length === 0 ? <div key="0">No users</div> :
 
-const Users = (props) => {
+  users.map((user) => {
+    const idUser = `user_${user.socket}`;
+    return <div key={idUser} className="chat-user text-primary strong">
+      <strong>{ user.name }</strong>
+    </div>
+  });
+
   return (
     <div id="user-wrap" className="col-sm-3">
       <div className="panel panel-default">
-        <div className="panel-heading">Active users</div>
+        <div className="panel-heading">{ heading }</div>
         <div className="panel-body">
           <div id="chat-users">
-            { noUsers }
-            <div className="chat-user text-primary strong" ng-repeat="user in users">
-              <strong>user.name</strong>
-            </div>
+            { divUsers }
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
+
+Users.propTypes = {
+  heading: PropTypes.string.isRequired,
+  users: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+Users.defaultProps = {
+  heading: 'Users',
+};
 
 export default Users;
